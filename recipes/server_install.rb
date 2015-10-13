45fdd27dee89a040a4f53f1b97590e836cf96007
Joe Miller <jmiller@paraport.com>
2015-10-13 14:03:55 -0700

    Removing failing slack plugin

--- recipes/server_install.rb
+++ recipes/server_install.rb
@@ -222,12 +222,3 @@ bags.each do |project|
     end
   end
 end
-
-# Plugins
-remote_file  'rundeck-slack-incoming-webhook-plugin' do
-  source node['rundeck']['plugin']['slack']
-  path "#{node['rundeck']['basedir']}/libext/rundeck-slack-incoming-webhook-plugin.jar"
-  owner node['rundeck']['user']
-  group node['rundeck']['group']
-  action :create
-end
